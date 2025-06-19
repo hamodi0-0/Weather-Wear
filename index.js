e34cd6fa639b0cdf9d3b71e30384d76878cb4f3c
hamodi0-0 <mh9988527@gmail.com>
2025-06-19 17:46:02 +0300

    main functions established

--- index.js
+++ index.js
@@ -5,10 +5,22 @@ import bodyParser from 'body-parser'
 
 const app = express();
 const PORT = 3000;
+const apiKey = '0af651eec33d3363ff66165763d40efa'
 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res)=>{
     res.render('index.ejs')
+})
+
+app.post('/', async (req, res)=>{
+    const {city} = req.body;
+    
+    const response = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=${apiKey}`)
+
+    const result = response.data
+    
+    
 })
 
 
